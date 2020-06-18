0a555ba6584564714e0b0aa9bdcd93feeec690a4
Maliha <maliha_786@hotmail.com>
2020-06-18 00:01:25 -0700

    fixed

--- app/main.py
+++ app/main.py
@@ -1,10 +1,10 @@
-from flask import Flask 
-  
-app = Flask(__name__) 
-  
-@app.route("/")
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/')
 def hello():
     return "Hello World!"
 
-def home_view(): 
-        return "<h1>Welcome to Geeks for Geeks</h1>"
+if __name__ == '__main__':
+    app.run()
